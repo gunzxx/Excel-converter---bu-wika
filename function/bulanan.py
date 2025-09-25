87c5d501f2a7877639f14f6bca0bac5d02c79e0c
Guntur W <ggm5850@gmail.com>
2025-09-26 04:32:30 +0700

    feat: update data

--- function/bulanan.py
+++ function/bulanan.py
@@ -52,30 +52,30 @@ def pdfToList(pages: list[page.Page]) -> list:
 
                 if row[0] and row[0].strip() != 'KODE' and row[0].lower() != 'T O T A L'.lower() and (not row[2] or row[2].strip() == ''):
                     if row[0].startswith('132'):
-                        rowData[5] = parseNumber(rowData[5]) + parseNumber(row[5])
-                        rowData[6] = toDefaultNumber(parseNumber(rowData[6]) + parseNumber(row[6]))
-                        total += parseNumber(row[6])
+                        rowData[5] = parseNumber(rowData[5]) + parseNumber(row[3])
+                        rowData[6] = toDefaultNumber(parseNumber(rowData[6]) + parseNumber(row[4]))
+                        total += parseNumber(row[4])
                     elif row[0].startswith('133'):
-                        rowData[7] = parseNumber(rowData[7]) + parseNumber(row[5])
-                        rowData[8] = toDefaultNumber(parseNumber(rowData[8]) + parseNumber(row[6]))
-                        total += parseNumber(row[6])
+                        rowData[7] = parseNumber(rowData[7]) + parseNumber(row[3])
+                        rowData[8] = toDefaultNumber(parseNumber(rowData[8]) + parseNumber(row[4]))
+                        total += parseNumber(row[4])
                     elif row[0].startswith('1341'):
-                        rowData[9] = parseNumber(rowData[9]) + parseNumber(row[5])
-                        rowData[10] = toDefaultNumber(parseNumber(rowData[10]) + parseNumber(row[6]))
-                        total += parseNumber(row[6])
+                        rowData[9] = parseNumber(rowData[9]) + parseNumber(row[3])
+                        rowData[10] = toDefaultNumber(parseNumber(rowData[10]) + parseNumber(row[4]))
+                        total += parseNumber(row[4])
                     elif row[0].startswith('135'):
-                        rowData[11] = parseNumber(rowData[11]) + parseNumber(row[5])
-                        rowData[12] = toDefaultNumber(parseNumber(rowData[12]) + parseNumber(row[6]))
-                        total += parseNumber(row[6])
+                        rowData[11] = parseNumber(rowData[11]) + parseNumber(row[3])
+                        rowData[12] = toDefaultNumber(parseNumber(rowData[12]) + parseNumber(row[4]))
+                        total += parseNumber(row[4])
                     # elif row[0].lower() in 'hak cipta, software, lisensi':
                     elif row[0].startswith('162') or 'aset tak berwujud' in row[0].lower():
-                        rowData[15] = parseNumber(rowData[15]) + parseNumber(row[5])
-                        rowData[16] = toDefaultNumber(parseNumber(rowData[16]) + parseNumber(row[6]))
-                        total += parseNumber(row[6])
+                        rowData[15] = parseNumber(rowData[15]) + parseNumber(row[3])
+                        rowData[16] = toDefaultNumber(parseNumber(rowData[16]) + parseNumber(row[4]))
+                        total += parseNumber(row[4])
                     elif '166112' in row[0].strip() or row[1].strip().lower() == 'aset tetap yang tidak digunakan dalam operasi pemerintahan'.lower():
-                        rowData[17] = parseNumber(rowData[17]) + parseNumber(row[5])
-                        rowData[18] = toDefaultNumber(parseNumber(rowData[18]) + parseNumber(row[6]))
-                        total += parseNumber(row[6])
+                        rowData[17] = parseNumber(rowData[17]) + parseNumber(row[3])
+                        rowData[18] = toDefaultNumber(parseNumber(rowData[18]) + parseNumber(row[4]))
+                        total += parseNumber(row[4])
                     else:
                         continue
                 else:
